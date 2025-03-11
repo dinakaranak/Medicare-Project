15c34b964ed06520789ce0afe9120c39f5f72366
dinakaranak <dinakaranak001@gmail.com>
2025-03-11 16:06:42 +0530

    initial stage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    {/* hero section */}
+    <>
+    <section className='hero_section pt-[60px] 2xl:h-[800px]'>
+      <div className='container'>
+        <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
+
+        </div>
+      </div>
+    </section>
+    </>
+    
+    
+    </>
   )
 }
 
